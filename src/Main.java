74621cd6c8e2df3055079c6f0aacc541eccb0483
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-04-29 20:18:39 +0200

    Zmiana na "Uczę się programowania i commitowania"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
         public static void main(String[] args) {
-            System.out.println("Uczę się programowania1212");
+            System.out.println("Uczę się programowania i commitowania");
         }
 }
