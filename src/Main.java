5735036638b46ae065f01896af592ae28f285d50
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-04-29 20:11:38 +0200

    Zmiana "Uczę się programowania1212"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
         public static void main(String[] args) {
-            System.out.println("Uczę się programowania");
+            System.out.println("Uczę się programowania1212");
         }
 }
